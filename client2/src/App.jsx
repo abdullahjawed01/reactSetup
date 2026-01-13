4e3ed2ade4a2a7ad6138da7506b8f067f17e19db
abdullahjawed01 <abdullahjawed2021@gmail.com>
2026-01-13 20:37:52 +0530

    Completed

---
+++ client2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='font-bold text-2xl bg-blue-500'>hello world from Abdullah </h1>
+    <p>this is my 2nd react </p>
+     
+    </>
+  )
+}
+
+export default App
---
+++ client3/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-center bg-amber-100 font-bold'>hello world form abdullah </h1>
+    <p className='text-center font-bold'>Hello this is my para</p>
+     
+    </>
+  )
+}
+
+export default App
